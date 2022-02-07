af0f926fa84344e9d6c8729421fb440082dc3034
Pablo Mochon <pimubiweb@gmail.com>
2022-02-07 20:33:22 +0100

    delete todo

--- todo-angular/src/app/components/todo-list/todo-list.component.ts
+++ todo-angular/src/app/components/todo-list/todo-list.component.ts
@@ -6,23 +6,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+
+  //declare variables
   todos = new Array();
+  todoTitle = '';
+  todoDescription = '';
+  todoDate = new Date().toDateString();
+  idTodo= 0;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.todos = [{
-      'id': 1,
-      'title': 'hola que tal'
-    },
-    {
-      'id': 2,
-      'title': 'adios'
-    },
-    {
-      'id': 3,
-      'title': 'otra cosa mas'
-    }]
+    this.todoTitle = '';
+    this.todoDescription = '';
+    this.idTodo = 1;
+    this.todos = [];
+    }
+
+    addTodo(): void{
+      if(this.todoTitle.trim().length === 0 ){
+        return;
+      }
+      if(this.todoDescription.trim().length === 0 ){
+        return;
+      }
+      this.todos.push({
+        id:this.idTodo,
+        title: this.todoTitle,
+        description: this.todoDescription,
+        date: this.todoDate,
+        completed: false
+      })
+      this.todoTitle = '';
+      this.todoDescription = '';
+      this.idTodo++;
+    }
+
+    deleteTodo(id: number): void {
+      const index = this.todos.findIndex((todo) => todo.id == id)
+        this.todos.splice(index, 1)
+        this.updateLocal();
+    }
+
+    changeCompleted():void { //change boolean value of todo.completed 
+      
+    }
+
+    //TODO dont work local storage
+    // local storage
+    // downloadLocal(): void{ 
+    //   if(JSON.parse(localStorage.getItem(this.LocalTodo)))
+    //       this.todos = JSON.parse(localStorage.getItem('todos'));
+    // }
+
+    //update local storage
+    updateLocal(): void{
+      localStorage.setItem('todos', JSON.stringify(this.todos))
     }
   }
 
